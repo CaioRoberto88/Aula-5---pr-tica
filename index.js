442299e9eb2d9e3c560e74e729362c1899573b87
Caio Roberto <>
2022-12-05 18:58:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let nome = "Caio Roberto";
+
+let salarioBruto = 7500;
+
+let ir = salarioBruto * 10 / 100;
+
+let salarioLiquido = salarioBruto - ir;
+
+console.log( `${nome} tem um salário líquido de R$ ${salarioLiquido}`);
