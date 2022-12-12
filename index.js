0aebec108a414be8532864630b5d31150aa3815c
Caio Roberto Dos Anjos  Santos <114942754+CaioRoberto88@users.noreply.github.com>
2022-12-12 16:44:02 +0000

    Aula - 2 - Semana 2 - Estrutura condicional alinhada.
    versão - 8

--- index.js
+++ index.js
@@ -1,44 +1,57 @@
-/* Problema - 1 - Semana 2  Javascript 
+/* Problema - 2 - Estrutura Condicional Alinhada
 
-Elabore um algoritimo para calcular o salário líquido de uma pessoa.
+1- Elabore um algoritmo para calcular o salário líquido de uma pessoa.
 
-Solicite ao usuário que digite seu nome e o valor de seu salário bruto.
+2- Solicite ao usuário que digite seu nome e o valor de seu salário bruto.
 
-O imposto de renda a ser descontado do salário bruto deve considerar as seguites regras:
+3- Peça também, para o usuário digitar a quantidade de dependentes.
 
-1- Salário bruto de ( R$ 0,00 até R$ 1.903,98 ): taxa de 5%
+4- Calcule a renda familiar per capta;
 
-2- Salário bruto de ( R$ 1.903,99 até  R$ 2.826,65 ) taxa de 7.5%
+5- Caso a renda para cada membro da família seja menor que R$ 500,00 a pessoa ficará isenta de imposto de renda, ou seja, não será calculado dentre as faixas salariais e seu valor será zero.
 
-3- Salário bruto acima de (R$ 2.826,66 ): taxa de 15%
+6 - Caso a renda para cada membro da família seja maior ou igual a R$ 500,00 o imposto de renda a ser descontado do salário bruto deve considerar as seguintes regras:
+
+  --> Salário bruto (de 0,00 até 1.903,98) taxa de 5%
+
+  --> Salário bruto (de 1.903,99 até 2.826,65) taxa de 7,5%
+
+  --> Salário bruto (a partir de 2.826,66) taxa dê 15%
+
+7- Ao final, mostre na tela o valor do salário líquido.
 
 */
 
-
-
 let prompt = require("prompt-sync")();
 
+let nome = prompt("Digite seu nome");
 
-let nome = prompt ("Digite seu nome");
+let salarioBruto = parseFloat(prompt("Digite seu salário bruto"));
 
-let salarioBruto = parseFloat(prompt("Digite o seu salário bruto"));
+let dependentes = prompt("Digite número de dependentes");
 
-if (salarioBruto > 0 && salarioBruto <= 1903.98) {
-  let ir5 = (salarioBruto * 5) / 100;
-  console.log(
-    `Senhor ${nome} o seu saláio liquido é de R$ ${salarioBruto - ir5
-    } seguindo a tabela da receita federal de taxar 5% de quem recebe até R$ 1.903,98`
-  );
-} else if (salarioBruto <= 2826.65) {
-  let ir7 = (salarioBruto * 7.5) / 100;
-  console.log(
-    `Senhor ${nome} o seu salário líquido é de R$ ${salarioBruto - ir7
-    } seguindo a tabela da receita federal de taxar 7.5% de quem recebe acima de R$ 1.903,99 e menos do que R$ 2.826,65`
-  );
-} else {
-  let ir15 = (salarioBruto * 15) / 100;
-  console.log(
-    `Senhor ${nome} o seu salário líquido é de R$ ${salarioBruto - ir15
-    } seguindo a tabela da receita federal de taxar 15% de quem recebe acima de R$ 2.826,65`
-  );
+let rendaPerCapita = salarioBruto / (dependentes + 1);
+
+
+if (rendaPerCapita <= 500) {
+  console.log(`${nome} o senhor tem renda per capta de ${rendaPerCapita}, por pessoa então o senhor ficará isento de pagar impostos, por tanto seu salario líquido será igual seu salário bruto que é de R${salarioBruto}`);
+} else if (rendaPerCapita >= 500) {
+  if (salarioBruto > 0 && salarioBruto <= 1903.98) {
+
+    let ir5 = salarioBruto * 5 / 100;
+
+    console.log(`${nome} o senhor tem Nº${dependentes} depedentes e seu salário bruto e de R$ ${salarioBruto}, e terá uma taxa de 5%, pois sua renda per capta é de ${rendaPerCapita} por pessoa, sendo assim seu salário líquido menos os descontos ficará em R$ ${salarioBruto - ir5}`);
+
+  } else if (salarioBruto <= 2826.65) {
+
+    let ir75 = salarioBruto * 7.5 / 100;
+
+    console.log(`${nome} o senhor tem Nº${dependentes} depedentes e seu salário bruto e de R$ ${salarioBruto}, e terá uma taxa de 7,5%, pois sua renda per capta é de ${rendaPerCapita}, sendo assim seu salário líquido menos os descontos ficará em R$ ${salarioBruto - ir75}`);
+
+  } else {
+    let ir15 = salarioBruto * 15 / 100;
+
+    console.log(`${nome} o senhor tem Nº${dependentes} depedentes e seu salário bruto e de R$ ${salarioBruto}, e terá uma taxa de 15%, pois sua renda per capta é de ${rendaPerCapita}, sendo assim seu salário líquido menos os descontos ficará em R$ ${salarioBruto - ir15}`);
+
+  }
 }
