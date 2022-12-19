91079e813257ca4e8e22aa2e79415da36405d32c
Caio Roberto Dos Anjos  Santos <114942754+CaioRoberto88@users.noreply.github.com>
2022-12-19 14:44:39 +0000

    versão 9 - loop for exercicio 1 - semana 2
    coloquei quebra de linha nos códigos.

--- index.js
+++ index.js
@@ -1,63 +1,51 @@
-let element = 5;
+let nome = prompt("Digite seu nome");
 
-let prompt = require("prompt-sync")
-();
+let salarioBruto = parseFloat(prompt("Digite seu salário bruto"));
 
-for (let index = 1; index <= element; index++) {
+let dependentes = parseInt(prompt("Numero de dependentes"));
 
-  const element = [index];
+let rendaFamiliar = parseInt(salarioBruto / (dependentes + 1));
 
-  console.log("Pessoa ", element);
+if (rendaFamiliar >= 500) {
+  if (salarioBruto > 0 && salarioBruto < 1903.98) {
+    let salarioLiquido = (salarioBruto * 5) / 100;
 
-  let nome = prompt("Digite seu nome");
-
-  let salarioBruto = parseFloat(prompt("Digite seu salário bruto"));
-  
-  let dependentes = parseInt(prompt("Numero de dependentes"));
-  
-  let rendaFamiliar = parseInt(salarioBruto / (dependentes + 1));
-  
-  if (rendaFamiliar >= 500) {
-    if (salarioBruto > 0 && salarioBruto < 1903.98) {
-      let salarioLiquido = (salarioBruto * 5) / 100;
-  
-      console.log(
-        "seu salário liquido é de R$",
-        salarioBruto - salarioLiquido,
-        "Você",
-        nome,
-        "foi taxado em 5% sobre seu salário bruto de R$",
-        salarioBruto
-      );
-    } else if (salarioBruto >= 1903.99 && salarioBruto <= 2826.65) {
-      let salarioLiquido = (salarioBruto * 7.5) / 100;
-  
-      console.log(
-        "seu salário liquido é de R$",
-        salarioBruto - salarioLiquido,
-        "Você",
-        nome,
-        "foi taxado em 7.5% sobre seu salário bruto de R$",
-        salarioBruto
-      );
-    } else {
-      let salarioLiquido = (salarioBruto * 15) / 100;
-  
-      console.log(
-        "seu salário liquido é de R$",
-        salarioBruto - salarioLiquido,
-        "Você",
-        nome,
-        "foi taxado em 15% sobre seu salário bruto de R$",
-        salarioBruto
-      );
-    }
-  } else {
     console.log(
-      "Renda familia de R$",
-      rendaFamiliar,
-      "reais por pessoa.",
-      "Por isso não será cobrado nenhuma taxa, motivo renda familiar abaixo de R$ 500 reais por pessoa"
+      "seu salário liquido é de R$",
+      salarioBruto - salarioLiquido,
+      "Você",
+      nome,
+      "foi taxado em 5% sobre seu salário bruto de R$",
+      salarioBruto,"\n"
+    );
+  } else if (salarioBruto >= 1903.99 && salarioBruto <= 2826.65) {
+    let salarioLiquido = (salarioBruto * 7.5) / 100;
+
+    console.log(
+      "seu salário liquido é de R$",
+      salarioBruto - salarioLiquido,
+      "Você",
+      nome,
+      "foi taxado em 7.5% sobre seu salário bruto de R$",
+      salarioBruto,"\n"
+    );
+  } else {
+    let salarioLiquido = (salarioBruto * 15) / 100;
+
+    console.log(
+      "seu salário liquido é de R$",
+      salarioBruto - salarioLiquido,
+      "Você",
+      nome,
+      "foi taxado em 15% sobre seu salário bruto de R$",
+      salarioBruto,"\n"
     );
   }
+} else {
+  console.log(
+    "Renda familia de R$",
+    rendaFamiliar,
+    "reais por pessoa.",
+    "Por isso não será cobrado nenhuma taxa, motivo renda familiar abaixo de R$ 500 reais por pessoa \n"
+  );
 }
